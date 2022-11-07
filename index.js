e12ad8093740623ca8fcd1065d34569a764b8263
Tsubasa.Masaki <mt@Tsubasa.Masaki>
2022-11-07 23:34:09 +0900

    save

--- index.js
+++ index.js
@@ -6,8 +6,20 @@ const typeDefs = `
     allPhotos: [Photo!]!
   }
 
-  type Mutation {
-    postPhoto(name: String! description: String): Photo!
+  enum PhotoCategory {
+    SELFIE
+    PORTRAIT
+    ACTION
+    LANDSCAPE
+    GRAPHIC
+  }
+
+  type User {
+    githubLogin: ID!
+    name: String
+    avator: String
+    postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
   }
 
   type Photo {
@@ -15,11 +27,60 @@ const typeDefs = `
     url: String!
     name: String!
     description: String
+    category: PhotoCategory!
+    postedBy: User!
+    taggedUsers: [User!]!
+  }
+
+  input PostPhotoInput {
+    name: String!
+    category: PhotoCategory=PORTRAIT
+    description: String
+  }
+
+  type Mutation {
+    postPhoto(input: PostPhotoInput!): Photo!
   }
 `;
 
 let _id = 0;
-let photos = [];
+
+let users = [
+  { githubLogin: "mHattrup", name: "Mike Hattrup" },
+  { githubLogin: "gPlake", name: "Glen Plake" },
+  { githubLogin: "sSchmidt", name: "Scot Schmidt" },
+];
+
+let photos = [
+  {
+    id: "1",
+    name: "Dropping the Heart Chute",
+    description: "The heart chute is one of my favorite chutes",
+    category: "ACTION",
+    githubUser: "gPlake",
+  },
+  {
+    id: "2",
+    name: "Enjoying the sunshine",
+    category: "SELFIE",
+    githubUser: "sSchmidt",
+  },
+  {
+    id: "3",
+    name: "Gunbarrel 25",
+    description: "25 laps on gunbarrel today",
+    category: "LANDSCAPE",
+    githubUser: "sSchmidt",
+  },
+];
+
+let tags = [
+  { photoID: "1", userID: "gPlake" },
+  { photoID: "2", userID: "sSchmidt" },
+  { photoID: "2", userID: "mHattrup" },
+  { photoID: "2", userID: "gPlake" },
+];
+
 const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
@@ -29,7 +90,7 @@ const resolvers = {
     postPhoto(parent, args) {
       let newPhoto = {
         id: _id++,
-        ...args,
+        ...args.input,
       };
       photos.push(newPhoto);
       return newPhoto;
@@ -37,6 +98,24 @@ const resolvers = {
   },
   Photo: {
     url: (parent) => `htttp://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: (parent) => {
+      return users.find((u) => u.githubLogin === parent.githubUser);
+    },
+    taggedUsers: (parent) =>
+      tags
+        .filter((tag) => tag.photoID === parent.id)
+        .map((tag) => tag.userID)
+        .map((userId) => users.find((u) => u.githubLogin === userId)),
+  },
+  User: {
+    postedPhotos: (parent) => {
+      return photos.filter((p) => p.githubUser == parent.githubLogin);
+    },
+    inPhotos: (parent) =>
+      tags
+        .filter((tag) => tag.userID === parent.id)
+        .map((tag) => tag.photoID)
+        .map((photoID) => photos.find((p) => p.id === photoID)),
   },
 };
 
